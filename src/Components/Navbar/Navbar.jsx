7d799588ce2c6fe6b0898958430c24d1c8b3cf5b
AMAN7040 <amansingh79620@gmail.com>
2025-02-21 01:14:52 +0530

    navbar sticky functionality modified

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       setPrevScroll((prev) => {
         const currentScroll = window.scrollY;
 
-        if (currentScroll > 118) {
+        if (currentScroll > 114) {
           setSticky(true);
           setHidden(currentScroll > prev);
         } else {
@@ -33,7 +33,7 @@ const Navbar = () => {
 
   return (
     <header
-      className={`relative z-4 h-18 border-b-1 border-gray-200 font-secondary text-sm text-primary transition-transform duration-300 ease-out ${isSticky ? styles.sticky : ""} ${isHidden ? styles.hidden : styles.visible}`}
+      className={`relative z-4 h-18 border-b-1 border-gray-200 font-secondary text-sm text-primary transition-transform duration-300 ease-out ${isSticky ? styles.navbarSticky : ""} ${isHidden ? styles.hidden : styles.visible}`}
     >
       {/* Smaller Screens */}
       <nav className="flex px-7.5 py-2.5 font-secondary text-primary lg:hidden">
