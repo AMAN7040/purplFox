c3eac57adf8871a238c1a78ec00440801d1d3669
AMAN7040 <amansingh79620@gmail.com>
2025-02-27 01:56:34 +0530

    Our Partners page setup

---
+++ src/Pages/OurPartners.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BreadCrumps from '../Components/BreadCrumps/BreadCrumps';
+
+const OurPartners = () => {
+  return (
+    <main>
+      <BreadCrumps/>
+    </main>
+  )
+}
+
+export default OurPartners;
