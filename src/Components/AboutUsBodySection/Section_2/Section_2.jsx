58cfdf9e896c3b50dc6cfb648efc9e57f10f716f
AMAN7040 <amansingh79620@gmail.com>
2025-02-28 21:16:04 +0530

    About Us section 2 updated

--- src/Components/AboutUsBodySection/Section_2/Section_2.jsx
+++ src/Components/AboutUsBodySection/Section_2/Section_2.jsx
@@ -8,11 +8,23 @@ const Section_2 = () => {
       <div className={`${styles.ctnrBanner} ${styles.bannerWrapper} relative`}>
         <div className="absolute top-0 left-0 h-full w-full px-[calc(var(--spacing)*2.5)] md:px-[calc(var(--spacing)*4)]">
           <div className="relative h-full w-full overflow-hidden">
-            <div className="block h-full w-full">
-              <img
-                src={image}
-                className="h-full w-full object-cover"
-              />
+            <div className="block h-full w-full brightness-90">
+              <img src={image} className="h-full w-full object-cover" />
+            </div>
+          </div>
+        </div>
+        <div
+          className={`${styles.subSect2} flex-center relative z-1 w-full flex-col px-[calc(var(--spacing)*2.5)] py-[calc(var(--spacing)*5)] text-center md:px-[calc(var(--spacing)*4)]`}
+        >
+          <div className="max-h-full w-full">
+            <div
+              className={`${styles.container2} m-auto max-h-full overflow-hidden text-center`}
+            >
+              <div className="mx-auto mt-[calc(var(--spacing)*3)] max-w-[650px]">
+                <h2 className="font-primary text-[27px] text-white xs:text-[30px] md:text-[35px]">
+                  <strong>The Creative Minds Behind Our Journey</strong>
+                </h2>
+              </div>
             </div>
           </div>
         </div>
